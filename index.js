ec6037f82e958f66684413d0719fbddad8a6f3a4
Emir <contactwme.451@gmail.com>
2021-10-24 00:21:53 +0300

    First Commits

---
+++ index.js
@@ -0,0 +1,30 @@
+// Calculator Image Link => https://i.imgur.com/1jDk1eo.png
+const RPC = require("discord-rpc");
+const client = new RPC.Client({
+    transport: "ipc"
+})
+const config = require("./config.json")
+
+client.on("ready", () => {
+    client.setActivity({
+        details: config.Details,
+        largeImageKey: config.LargeImage,
+        largeImageText: config.LargeImageText,
+        buttons:[
+            {
+                label: config.Button1,
+                url:config.Link1
+            },
+            {
+                label: config.Button2,
+                url: config.Link2
+            }
+        ]
+    })
+
+    console.log(`RPC is active, authed for the user ${client.user.username}#${client.user.discriminator} (${client.user.id})`);
+})
+
+client.login({
+    clientId: config.ClientID
+})
